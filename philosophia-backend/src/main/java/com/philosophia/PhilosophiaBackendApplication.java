00c152739bc05f11c97e5a36205a0bfafddfca25
fedi-afli <f3diafli@gmail.com>
2026-08-31 18:31:14 +0100

    project structure

---
+++ philosophia-backend/src/main/java/com/philosophia/PhilosophiaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.philosophia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhilosophiaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhilosophiaBackendApplication.class, args);
+	}
+
+}
---
+++ philosophia-backend/src/test/java/com/philosophia/PhilosophiaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.philosophia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhilosophiaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
